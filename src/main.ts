ae5930d4d652888b6c9cb3baaa2b3615d61a389b
harunkurtdev <harunkurt.dev@gmail.com>
2024-11-02 00:59:29 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
